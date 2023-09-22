516cdb666f6f9f2b708176dad780aa6739b35227
Mr.Perfect 092 <135037455+stephan-maina@users.noreply.github.com>
2023-09-22 23:31:24 +0300

    Completed Lab

--- server/models.py
+++ server/models.py
@@ -1,8 +1,35 @@
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import MetaData
 
+# Create a custom naming convention for SQLAlchemy
 metadata = MetaData(naming_convention={
     "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
 })
 
+# Create a SQLAlchemy instance with the custom naming convention
 db = SQLAlchemy(metadata=metadata)
+
+# Define the Owner model
+class Owner(db.Model):
+    __tablename__ = 'owners'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String, unique=True)
+
+    pets = db.relationship('Pet', backref='owner')
+
+    def __repr__(self):
+        return f'<Pet Owner {self.name}>'
+
+# Define the Pet model
+class Pet(db.Model):
+    __tablename__ = 'pets'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+    species = db.Column(db.String)
+
+    owner_id = db.Column(db.Integer, db.ForeignKey('owners.id'))
+
+    def __repr__(self):
+        return f'<Pet {self.name}, {self.species}>'
